402689ca3582821be26a3c66f48e43332fb1da08
Arash <arash8877@gmail.com>
2025-07-30 09:40:29 +0200

    add UserAccount test

---
+++ tests/UserAccount.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+import { User } from "../src/entities";
+import UserAccount from "../src/components/UserAccount";
+
+describe("UserAccount", () => {
+  it("should render user name", () => {
+    const user: User = { id: 1, name: "Arash" };
+
+    render(<UserAccount user={user} />);
+
+    expect(screen.getByText(user.name)).toBeInTheDocument();
+  });
+
+  it("should render edit button if user is admin", () => {
+    const user: User = { id: 1, name: "Arash", isAdmin: true };
+
+    render(<UserAccount user={user} />);
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/Edit/i);
+  });
+
+  it("should not render edit button if user is not admin", () => {
+    const user: User = { id: 1, name: "Arash", isAdmin: false };
+
+    render(<UserAccount user={user} />);
+
+    const button = screen.queryByRole("button");
+    expect(button).not.toBeInTheDocument();
+  });
+});
