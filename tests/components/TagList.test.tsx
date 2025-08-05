ae0c62aab24a8a0cc3efb5a541636d4ed62c9ccd
Arash <arash8877@gmail.com>
2025-08-05 11:34:35 +0200

    test tagList component done

--- tests/components/TagList.test.tsx
+++ tests/components/TagList.test.tsx
@@ -1,13 +1,19 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import TagList from "../../src/components/TagList";
 
-
 // this test, testing an async component that fetches data
-// TagList component includes an async function 
+// TagList component includes an async function
 
-describe('TagList', () => {
-    it('should render tags', () => {
-        render (<TagList />);
-        
-    })
-})
+describe("TagList", () => {
+  it("should render tags", async () => {
+    render(<TagList />);
+
+    // await waitFor(() => {
+    //   const listItems = screen.getAllByRole("listitem");
+    //   expect(listItems.length).toBeGreaterThan(0);
+    // });
+
+    const listItems = await screen.findAllByRole("listitem"); // findAllByRole is a combination of waitFor() and getAllByRole 
+    expect(listItems.length).toBeGreaterThan(0);
+  });
+});
