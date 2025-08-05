e887143fa174629b22ffb60f1f78c54fb785716a
Arash <arash8877@gmail.com>
2025-08-05 17:31:18 +0200

    refactor OrderStatusSelector.test

--- tests/components/OrderStatusSelector.test.tsx
+++ tests/components/OrderStatusSelector.test.tsx
@@ -4,29 +4,62 @@ import { Theme } from "@radix-ui/themes";
 import userEvent from "@testing-library/user-event";
 
 describe("OrderStatusSelector", () => {
-  it("should render New as the default value", () => {
+  // it("should render New as the default value", () => {
+  //   render(
+  //     <Theme>
+  //       <OrderStatusSelector onChange={vi.fn()} />
+  //     </Theme>
+  //   );
+  //   const button = screen.getByRole("combobox");
+  //   expect(button).toHaveTextContent("New");
+  // });
+
+  // it("should render correct statuses", async () => {
+  //   render(
+  //     <Theme>
+  //       <OrderStatusSelector onChange={vi.fn()} />
+  //     </Theme>
+  //   );
+  //   const button = screen.getByRole("combobox");
+  //   const user = userEvent.setup();
+  //   await user.click(button);
+
+  //   const options = await screen.findAllByRole("option");
+  //   expect(options).toHaveLength(3);
+  //   const labels = options.map(option => option.textContent);
+  //   expect(labels).toEqual(["New", "Processed", "Fulfilled"]);
+  // });
+
+  const renderComponent = () => {
     render(
       <Theme>
         <OrderStatusSelector onChange={vi.fn()} />
       </Theme>
     );
-    const button = screen.getByRole("combobox");
-    expect(button).toHaveTextContent("New");
+
+    return {
+      dropDown: screen.getByRole("combobox"),
+      getOptions: ()=> screen.getAllByRole("option"),
+    };
+  };
+
+  it("should render New as the default value", () => {
+    const { dropDown } = renderComponent();
+    expect(dropDown).toHaveTextContent("New");
   });
 
-  it("should render correct statuses", async () => {
-    render(
-      <Theme>
-        <OrderStatusSelector onChange={vi.fn()} />
-      </Theme>
-    );
-    const button = screen.getByRole("combobox");
-    const user = userEvent.setup();
-    await user.click(button);
 
-    const options = await screen.findAllByRole("option");
+    it("should render correct statuses", async () => {
+  const { dropDown, getOptions } = renderComponent();
+    const user = userEvent.setup();
+    await user.click(dropDown);
+
+    const options = await getOptions();
     expect(options).toHaveLength(3);
     const labels = options.map(option => option.textContent);
     expect(labels).toEqual(["New", "Processed", "Fulfilled"]);
   });
+
+
+
 });
