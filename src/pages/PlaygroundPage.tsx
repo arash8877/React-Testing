bc39d619603df3dc4b6c3e00af75b6a54406d8b0
Arash <arash8877@gmail.com>
2025-08-05 13:23:18 +0200

    testing library componnet

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -3,19 +3,21 @@
 // import ExpandableText from "../components/ExpandableText";
 // import SearchBox from "../components/SearchBox";
 // import TagList from "../components/TagList";
-
-import { Toaster } from "react-hot-toast";
-import ToastDemo from "../components/ToastDemo";
+// import { Toaster } from "react-hot-toast";
+// import ToastDemo from "../components/ToastDemo";
+import OrderStatusSelector from "../components/OrderStatusSelector";
 
 const PlaygroundPage = () => {
   return (
     // <ExpandableText text="  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla nostrum quaerat nesciunt fugiat. Pariatur iusto veritatis cupiditate tempora eius modi nihil placeat provident eos similique culpa ab sapiente, nemo, natus libero rem voluptatibus facere suscipit! Eveniet molestias a tenetur, repudiandae eaque minima. In, eveniet! Labore deserunt voluptates laborum, veniam porro animi inventore eius dolor, quisquam deleniti esse fugiat nemo hic natus doloribus nulla enim consequatur distinctio pariatur dolore, expedita quod quasi. Autem tempore dignissimos ab minus optio deserunt impedit neque qui consectetur, minima ullam, expedita dicta praesentium repudiandae temporibus harum mollitia? Autem illum corrupti natus ipsum cupiditate eius corporis ut." />
     // <SearchBox onChange={(text) => alert(text)} />
     // <TagList />
-    <>
-      <ToastDemo />
-      <Toaster />
-    </>
+    // <>
+    //   <ToastDemo />
+    //   <Toaster />
+    // </>
+
+    <OrderStatusSelector onChange={(value) => alert(value)} />
   );
 };
 
