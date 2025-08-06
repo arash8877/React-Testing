f7567ecdfce0d378bf23c6637b2521c90dd7b053
Arash <arash8877@gmail.com>
2025-08-06 14:44:24 +0200

    test data fetching in ProductList

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -8,4 +8,18 @@ export const handlers = [
       { id: 3, name: "Gardening" },
     ]);
   }),
+
+  http.get('/products', ()=> {
+    return HttpResponse.json([
+      { id: 1, name: 'Product 1'},
+      { id: 2, name: 'Product 2'},
+      { id: 3, name: 'Product 3'},
+    ])
+  })
+
+
+
+  // http.get("/products/:id", ({params}) => {
+  //   const {id} = params;
+  // })
 ];
