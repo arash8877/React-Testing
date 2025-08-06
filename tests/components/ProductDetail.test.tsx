b88f340d97c8688fab16304dfe7b146ffe6ae1c2
Arash <arash8877@gmail.com>
2025-08-06 15:43:17 +0200

    ProductDetail test done

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import ProductDetail from "../../src/components/ProductDetail";
 import { products } from "../mocks/data";
+import { server } from "../mocks/server";
+import { HttpResponse, http } from "msw";
 
 describe("ProductDetail", () => {
   it("should render the list of products", async () => {
@@ -12,5 +14,24 @@ describe("ProductDetail", () => {
 
     expect(await screen.findByText(new RegExp(products[0].name))).toBeInTheDocument();
     expect(await screen.findByText(new RegExp(products[0].price.toString()))).toBeInTheDocument();
+  });
+
+  it("should render a message if product is not found", async() => {
+    server.use(http.get("/products/1", () => HttpResponse.json(null)));
+
+    render(<ProductDetail productId={1} />);
+
+    const message = await screen.findByText(/not found/i);
+
+    expect(message).toBeInTheDocument();
+  });
+
+  it("should render an error for invalid productId ", async() => {
+
+    render(<ProductDetail productId={0} />);
+
+    const message = await screen.findByText(/invalid/i);
+
+    expect(message).toBeInTheDocument();
   });
 });
