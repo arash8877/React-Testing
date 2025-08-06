f79445066d98aaa4b9c481b757e91ea80a6977fb
Arash <arash8877@gmail.com>
2025-08-06 10:01:26 +0200

    setting up the mock-service-worker

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,5 +1,10 @@
-import '@testing-library/jest-dom/vitest';
-import ResizeObserver from 'resize-observer-polyfill';
+import "@testing-library/jest-dom/vitest";
+import ResizeObserver from "resize-observer-polyfill";
+import { server } from "./mocks/server";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 global.ResizeObserver = ResizeObserver;
 
@@ -7,16 +12,16 @@ window.HTMLElement.prototype.scrollIntoView = vi.fn();
 window.HTMLElement.prototype.hasPointerCapture = vi.fn();
 window.HTMLElement.prototype.releasePointerCapture = vi.fn();
 
-Object.defineProperty(window, 'matchMedia', {
-    writable: true,
-    value: vi.fn().mockImplementation(query => ({
-      matches: false,
-      media: query,
-      onchange: null,
-      addListener: vi.fn(), // deprecated
-      removeListener: vi.fn(), // deprecated
-      addEventListener: vi.fn(),
-      removeEventListener: vi.fn(),
-      dispatchEvent: vi.fn(),
-    })),
-  })
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
